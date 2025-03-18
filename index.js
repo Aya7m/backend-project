5a664f61732e2996c5619b373ef6830a76939044
Aya7m <yyoyo6987@gmail.com>
2025-03-18 21:10:28 +0200

    add

--- index.js
+++ index.js
@@ -8,14 +8,9 @@ import cartRouter from './routers/cart.route.js'
 import coupenRouter from './routers/coupen.routes.js'
 import paymentRoute from './routers/payment.route.js'
 import analysisRouter from './routers/analytics.route.js'
-import cors from 'cors'
 
-const corsOptions = {
-        origin: 'http://localhost:5173',
-        credentials: true,            //access-control-allow-credentials:true
-        optionSuccessStatus: 200
-    }
-    app.use(cors(corsOptions))
+
+
 
 
 
