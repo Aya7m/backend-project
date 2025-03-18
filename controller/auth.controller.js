58a778ee1bc82070477a7cb2e6d4026f78e58f9d
Aya7m <yyoyo6987@gmail.com>
2025-03-18 22:33:23 +0200

    hi

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,61 +3,70 @@ import User from "../model/user.model.js";
 import bcrypt from "bcryptjs"
 
 export const signUp = async (req, res) => {
+    try {
 
-    const { name, email, password } = req.body
+        const { name, email, password } = req.body
 
-    if (!name || !email || !password) {
-        return res.status(400).json({
-            success: false,
-            message: "All fields are required"
-        })
-    }
-
-    //    check email rejex
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        return res.status(400).json({ error: 'Invalid email format' });
-    }
-
- 
-  
-    // check email exist
-    const emailExist = await User.findOne({ email })
-    if (emailExist) {
-        return res.status(400).json({ error: 'Email already exist' });
-    }
-
-    // check password length
-    if (password.length < 6) {
-        return res.status(400).json({ error: 'Password must be at least 6 characters' });
-    }
-
-
-    // hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
-
-    // create user
-    const NewUser = await User.create({
-        
-        name,
-        email,
-        password: hashPassword,
-    });
-
-    if (NewUser) {
-        generateTokenAndSetCookie(NewUser._id, res)
-        await NewUser.save()
-        return res.status(201).json({
-            _id: NewUser._id,
-            name: NewUser.name,
-            email: NewUser.email,
-            role: NewUser.role
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+    
+        //    check email rejex
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: 'Invalid email format' });
+        }
+    
+     
+      
+        // check email exist
+        const emailExist = await User.findOne({ email })
+        if (emailExist) {
+            return res.status(400).json({ error: 'Email already exist' });
+        }
+    
+        // check password length
+        if (password.length < 6) {
+            return res.status(400).json({ error: 'Password must be at least 6 characters' });
+        }
+    
+    
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+    
+        // create user
+        const NewUser = await User.create({
+            
+            name,
+            email,
+            password: hashPassword,
         });
+    
+        if (NewUser) {
+            generateTokenAndSetCookie(NewUser._id, res)
+            await NewUser.save()
+            return res.status(201).json({
+                _id: NewUser._id,
+                name: NewUser.name,
+                email: NewUser.email,
+                role: NewUser.role
+            });
+        }
+        else {
+            return res.status(400).json({ error: 'User not created' });
+        }
+    
+        
+    } catch (error) {
+        console.log("Error in signUp controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+        
     }
-    else {
-        return res.status(400).json({ error: 'User not created' });
-    }
+
 
 
 
--- index.js
+++ index.js
@@ -24,20 +24,20 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 dotenv.config()
 const port = process.env.PORT || 5000
 
-app.use(express.json({limit:"50mb"}))
+app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
 
 
 app.use('/auth', authRouter)
-app.use('/product',productRoute)
-app.use('/cart',cartRouter)
-app.use('/coupon',coupenRouter)
-app.use('/payment',paymentRoute)
-app.use('/analysis',analysisRouter)
+app.use('/product', productRoute)
+app.use('/cart', cartRouter)
+app.use('/coupon', coupenRouter)
+app.use('/payment', paymentRoute)
+app.use('/analysis', analysisRouter)
 app.options('*', cors()); // السماح لجميع طلبات OPTIONS
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
