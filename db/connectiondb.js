0d05d902d6b3a7328d069234a81b1f504a8d80a9
Aya7m <yyoyo6987@gmail.com>
2025-03-19 13:05:42 +0200

    edit mongodb

--- db/connectiondb.js
+++ db/connectiondb.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.mongodb);
-        console.log("Connected to MongoDB");
+        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        
+        console.log("Connected to MongoDB ...");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
