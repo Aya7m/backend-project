243dba7ed7f28e25896dc871ec4060e95bb5f422
Aya7m <yyoyo6987@gmail.com>
2025-03-28 22:08:07 +0200

    add routepassword

--- routers/auth.route.js
+++ routers/auth.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  getUser, logout, signIn, signUp } from "../controller/auth.controller.js";
+import {  changePassword, getUser, logout, signIn, signUp } from "../controller/auth.controller.js";
 import { protectRoute } from "../middleware/auth.midleware.js";
 
 const authRouter=Router()
@@ -8,6 +8,7 @@ authRouter.post('/signup', signUp)
 authRouter.post('/signin', signIn)
 authRouter.post('/logout', logout)
 authRouter.get('/profile',protectRoute, getUser)
+authRouter.post('/changePassword',protectRoute,changePassword)
 
 
 export default authRouter
